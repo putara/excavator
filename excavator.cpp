6a0f177f4558961d45acfc9d6367c0033709c6a6
putara <38089082+putara@users.noreply.github.com>
2019-03-21 01:01:20 +1300

    Support fragmented directory entries

--- excavator.cpp
+++ excavator.cpp
@@ -10,6 +10,8 @@ excavator.obj: excavator.cpp
 
 #include <stdio.h>
 #include <stdint.h>
+#include <malloc.h>
+#include <conio.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -122,65 +124,79 @@ public:
             this->fd = -1;
         }
     }
-    bool open(const wchar_t* path, bool rw = false)
+    bool open(const wchar_t* path, bool write = false)
     {
         this->close();
-        return (this->fd = rw ? ::_wopen(path, O_WRONLY | O_BINARY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE) : ::_wopen(path, O_BINARY | O_RDONLY, S_IREAD)) != -1;
+        return (this->fd = write ? ::_wopen(path, O_WRONLY | O_BINARY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE) : ::_wopen(path, O_BINARY | O_RDONLY, S_IREAD)) != -1;
+    }
+    bool reopen(const File& src, bool write = false)
+    {
+        this->close();
+        if (src.fd == -1) {
+            return false;
+        }
+        HANDLE srchf = reinterpret_cast<HANDLE>(::_get_osfhandle(src.fd));
+        if (srchf == INVALID_HANDLE_VALUE) {
+            return false;
+        }
+        HANDLE dsthf = write ? ::ReOpenFile(srchf, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0) : ::ReOpenFile(srchf, GENERIC_READ, FILE_SHARE_READ, 0);
+        if (dsthf == INVALID_HANDLE_VALUE) {
+            return false;
+        }
+        if ((this->fd = ::_open_osfhandle(reinterpret_cast<intptr_t>(dsthf), O_BINARY)) == -1) {
+            ::CloseHandle(dsthf);
+            return false;
+        }
+        return true;
     }
     int64_t size() const
     {
         return ::_filelengthi64(this->fd);
     }
-    bool read_at(uint64_t offset, __out_bcount(cb) void* buffer, size_t cb) const
+    bool seek_at(uint64_t offset) const
     {
-        ::memset(buffer, 0, cb);
-        if (::_lseeki64(this->fd, static_cast<int64_t>(offset), SEEK_SET) == -1) {
-            return false;
+        return ::_lseeki64(this->fd, static_cast<int64_t>(offset), SEEK_SET) == static_cast<int64_t>(offset);
+    }
+    bool read_all_at(uint64_t offset, __out_bcount(cb) void* buffer, unsigned int cb) const
+    {
+        return this->read_n_at(offset, buffer, cb) == cb;
+    }
+    unsigned int read_n_at(uint64_t offset, __out_bcount(cb) void* buffer, unsigned int cb) const
+    {
+        if (this->seek_at(offset) == false) {
+            ::memset(buffer, 0, cb);
+            return 0;
         }
-        return static_cast<size_t>(::_read(this->fd, buffer, cb)) == cb;
+        return this->read_n(buffer, cb);
     }
-    bool read(__out_bcount(cb) void* buffer, size_t cb) const
+    bool read_all(__out_bcount(cb) void* buffer, unsigned int cb) const
+    {
+        return this->read_n(buffer, cb) == cb;
+    }
+    unsigned int read_n(__out_bcount(cb) void* buffer, unsigned int cb) const
     {
         ::memset(buffer, 0, cb);
-        return static_cast<size_t>(::_read(this->fd, buffer, cb)) == cb;
+        const int ret = ::_read(this->fd, buffer, cb);
+        return ret < 0 ? 0 : static_cast<unsigned int>(ret);
     }
-    size_t read_part(__out_bcount(cb) void* buffer, size_t cb, size_t cbBlock) const
+    unsigned int read_part(__out_bcount(cb) void* buffer, unsigned int cb, unsigned int cbBlock) const
     {
-        ::memset(buffer, 0, cb);
-        const size_t cbRead = static_cast<size_t>(::_read(this->fd, buffer, cb));
-        ::_lseeki64(this->fd, cbBlock - cbRead, SEEK_CUR);
-        return cbRead;
+        const unsigned int cbRead = this->read_n(buffer, cb);
+        return ::_lseeki64(this->fd, cbBlock - cbRead, SEEK_CUR) != -1 ? cbRead : 0;
+    }
+    unsigned int write_n(__in_bcount(cb) const void* buffer, unsigned int cb) const
+    {
+        const int ret = ::_write(this->fd, buffer, cb);
+        return ret < 0 ? 0 : static_cast<unsigned int>(ret);
     }
     bool reset() const
     {
-        return ::_lseeki64(this->fd, 0, SEEK_SET) == 0;
-    }
-    bool copy_to(const File& dst, uint64_t offset, uint64_t cb) const
-    {
-        if (::_lseeki64(this->fd, static_cast<int64_t>(offset), SEEK_SET) == -1) {
-            return false;
-        }
-        const size_t cbBuff = 512;
-        uint8_t buffer[cbBuff];
-        while (cb > 0) {
-            ::memset(buffer, 0, cbBuff);
-            size_t cbToRead = cb < cbBuff ? static_cast<size_t>(cb) : cbBuff;
-            size_t cbRead = static_cast<size_t>(::_read(this->fd, buffer, cbToRead));
-            if (cbRead == 0 || cbRead > cbToRead) {
-                return false;
-            }
-            size_t cbWritten = static_cast<size_t>(::_write(dst.fd, buffer, cbRead));
-            if (cbWritten != cbRead) {
-                return false;
-            }
-            cb -= cbRead;
-        }
-        return true;
+        return this->seek_at(0);
     }
     static bool setfileinfo(bool dir, const wchar_t* path, uint32_t attributes, int64_t created, int64_t accessed, int64_t modified)
     {
         bool result = false;
-        HANDLE hf = CreateFileW(path, FILE_WRITE_ATTRIBUTES, 0, NULL, OPEN_EXISTING, dir ? FILE_FLAG_BACKUP_SEMANTICS : 0, NULL);
+        HANDLE hf = ::CreateFileW(path, FILE_WRITE_ATTRIBUTES, 0, NULL, OPEN_EXISTING, dir ? FILE_FLAG_BACKUP_SEMANTICS : 0, NULL);
         if (hf != INVALID_HANDLE_VALUE) {
             FILE_BASIC_INFO info = {};
             info.CreationTime.QuadPart      = created;
@@ -290,6 +306,137 @@ private:
         uint8_t second; // 0-59
     };
 
+    class exFatFile
+    {
+    private:
+        const exFat& owner;
+        File file;
+        uint32_t cluster;
+        uint8_t* buff;
+        rsize_t pos;
+        size_t cbFilled;
+        size_t cbBuff;
+        bool chain;
+
+        exFatFile(const exFatFile&);
+        exFatFile& operator =(const exFatFile&);
+
+        uint32_t get_next_cluster() const
+        {
+            if (this->chain == false) {
+                return this->cluster + 1;
+            }
+            const uint64_t fat_offset = this->owner.get_fat_offset(this->cluster);
+            uint32_t next_cluster = 0;
+            if (this->file.read_all_at(fat_offset, &next_cluster, sizeof(next_cluster)) == false) {
+                return 0;
+            }
+            if (next_cluster > 0xfffffff6 || next_cluster <= 1) {
+                if (next_cluster == 0xfffffff7) {
+                    fprintf(stderr, "0x%08X: Bad cluster\n", next_cluster);
+                } else if (next_cluster == 0xfffffff8) {
+                    fprintf(stderr, "0x%08X: Media descriptor\n", next_cluster);
+                } else if (next_cluster == 0xffffffff) {
+                    //fprintf(stderr, "0x%08X: End of file\n", next_cluster);
+                } else {
+                    fprintf(stderr, "0x%08X\n", next_cluster);
+                }
+                return 0;
+            }
+            return next_cluster;
+        }
+        bool refill()
+        {
+            this->cbFilled = this->pos = 0;
+            if (this->cluster == 0) {
+                return false;
+            }
+            const uint64_t offset = this->owner.cluster_to_offset(this->cluster);
+            this->cluster = this->get_next_cluster();
+            return (this->cbFilled = this->file.read_n_at(offset, this->buff, this->cbBuff)) > 0;
+        }
+        bool open_worker(uint32_t cluster, bool chain)
+        {
+            this->file.close();
+            this->cluster = cluster;
+            this->cbFilled = this->pos = 0;
+            this->chain = chain;
+            if (this->buff == NULL && (this->buff = static_cast<uint8_t*>(::malloc(this->cbBuff))) == NULL) {
+                return false;
+            }
+            return this->file.reopen(this->owner.file, false);
+        }
+
+    public:
+        exFatFile(const exFat& self)
+            : owner(self)
+            , cluster()
+            , buff()
+            , pos(0)
+            , cbFilled(0)
+            , cbBuff(self.cluster_size)
+            , chain()
+        {
+        }
+        ~exFatFile()
+        {
+            ::free(this->buff);
+        }
+        bool open(uint32_t cluster)
+        {
+            return this->open_worker(cluster, false);
+        }
+        bool open(const stream_extension& se)
+        {
+            return this->open_worker(se.first_cluster, (se.flags & 2) == 0);
+        }
+        size_t read(void* buffer, size_t cb)
+        {
+            uint8_t* p = static_cast<uint8_t*>(buffer);
+            if (p != NULL) {
+                ::memset(p, 0, cb);
+            }
+            size_t cbRead = 0;
+            while (cb > 0) {
+                if (this->pos >= this->cbFilled) {
+                    if (this->refill() == false) {
+                        break;
+                    }
+                }
+                size_t cbToRead = this->cbFilled - this->pos;
+                if (cbToRead > cb) {
+                    cbToRead = cb;
+                }
+                if (p != NULL) {
+                    ::memcpy(p, this->buff + this->pos, cbToRead);
+                    p += cbToRead;
+                }
+                this->pos += cbToRead;
+                cb -= cbToRead;
+                cbRead += cbToRead;
+            }
+            return cbRead;
+        }
+        bool copy_to(const File& dst, uint64_t cb)
+        {
+            const unsigned int SIZE = 4096;
+            uint8_t buffer[SIZE];
+            while (cb > 0) {
+                const size_t cbToRead = cb < SIZE ? static_cast<size_t>(cb) : SIZE;
+                const size_t cbRead = this->read(buffer, cbToRead);
+                if (cbRead == 0 || cbRead > cbToRead) {
+                    return false;
+                }
+                const size_t cbWritten = dst.write_n(buffer, cbRead);
+                if (cbWritten != cbRead) {
+                    return false;
+                }
+                cb -= cbRead;
+            }
+            return true;
+        }
+    };
+
     uint32_t cluster_size;
     uint32_t reserved_sectors;
     uint32_t upcase_cluster;
@@ -314,6 +461,10 @@ private:
         tm.minute = (datetime >> 5) & 0x3f;
         tm.second = (datetime & 0x1f) * 2;
         return tm;
+    }
+    uint64_t get_fat_offset(uint32_t cluster) const
+    {
+        return static_cast<uint64_t>(this->fat_cluster) * this->cluster_size + static_cast<uint64_t>(cluster) * 4;
     }
     uint64_t cluster_to_offset(uint32_t cluster) const
     {
@@ -442,37 +593,45 @@ public:
         }
         this->upcase_cluster = this->find_upcase_table();
         if (this->upcase_cluster == 0) {
-            fprintf(stderr, "no upcase\n");
+            fprintf(stderr, "No upcase table found\n");
             return false;
         }
         this->fat_cluster = this->find_fat_table(this->upcase_cluster);
         if (this->fat_cluster == 0) {
-            fprintf(stderr, "no fat\n");
+            fprintf(stderr, "No file allocation table found\n");
             return false;
         }
         root_entry root = {};
-        this->root_cluster = this->find_root(&root);
-        if (this->root_cluster == 0) {
-            fprintf(stderr, "no root\n");
+        const uint32_t root_cluster = this->find_root(&root);
+        if (root_cluster == 0) {
+            fprintf(stderr, "No root directory found\n");
             return false;
         }
         if (this->reserved_sectors == 0) {
             this->reserved_sectors = this->upcase_cluster - root.upc.first_cluster + 2;
         }
+        if (root_cluster <= this->reserved_sectors) {
+            fprintf(stderr, "No root directory found\n");
+            return false;
+        }
+        this->root_cluster = root_cluster - this->reserved_sectors + 2;
         char buff[128];
         sprintf_s(buff, 128, "reserved_sectors: %u, cluster_size: %u, fat_cluster: %u, upcase_cluster: %u, %u, root_cluster: %08I64x (%u)\n",
             reserved_sectors, cluster_size,
-            fat_cluster, upcase_cluster, root.upc.first_cluster, static_cast<uint64_t>(root_cluster) * cluster_size, root_cluster);
-        fprintf(stderr, "%s", buff);
-        //printf("%s", buff);
+            fat_cluster, upcase_cluster, root.upc.first_cluster, static_cast<uint64_t>(root_cluster) * cluster_size, this->root_cluster);
+        _cprintf("%s", buff);
         return true;
     }
     void enum_files(const wchar_t* outdir, const wchar_t* pattern) const
     {
+        exFatFile dir(*this);
+        if (dir.open(this->root_cluster) == false || dir.read(NULL, sizeof(root_entry)) != sizeof(root_entry)) {
+            return;
+        }
         printf(
-        "   Date      Time    Attr     Entry Offset         Size       Offset      File Offset  Path\n"
-        "------------------- ----- ---------------- ------------ ------------ ----------------  ------------------------\n");
-        this->enum_files_worker(L"", outdir, pattern, static_cast<uint64_t>(this->root_cluster) * this->cluster_size + sizeof(root_entry), 0);
+        "   Date      Time    Attr         Size      Cluster      File Offset  Path\n"
+        "------------------- ----- ------------ ------------ ----------------  ------------------------\n");
+        this->enum_files_worker(dir, L"", outdir, pattern, 0);
     }
 
 private:
@@ -529,62 +688,28 @@ private:
         ::wcscpy_s(outname, outdir);
         ::wcscat_s(outname, path);
         mkdirs_to_file(outname);
-        bool result = false;
         File out;
         if (out.open(outname, true) == false) {
             return false;
         }
-        const uint64_t file_offset = this->cluster_to_offset(se.first_cluster);
-        if ((se.flags & 2) == 2) {
-            // normal file
-            result = this->file.copy_to(out, file_offset, se.data_length);
-        } else {
-            // fragmented file; copy only first cluster
-            uint64_t remains = se.data_length;
-            uint64_t size = remains < this->cluster_size ? remains : this->cluster_size;
-            result = this->file.copy_to(out, file_offset, size);
-            remains -= size;
-            // now look into the file allocation table
-            for (uint32_t cluster = se.first_cluster; result && remains > 0; ) {
-                if (cluster >= 0xfffffff7 || cluster <= 1) {
-                    if (cluster == 0xfffffff7) {
-                        fprintf(stderr, "Media descriptor?\n");
-                    } else if (cluster == 0xfffffff7) {
-                        fprintf(stderr, "Bad cluster?\n");
-                    } else if (cluster == 0xffffffff) {
-                        fprintf(stderr, "End of file?\n");
-                    } else {
-                        fprintf(stderr, "0x%08X?\n", cluster);
-                    }
-                    result = false;
-                    break;
-                }
-                const uint64_t fat_offset = static_cast<uint64_t>(this->fat_cluster) * cluster_size + static_cast<uint64_t>(cluster) * 4;
-                uint32_t next_cluster = 0;
-                result = this->file.read_at(fat_offset, &next_cluster, sizeof(next_cluster));
-                if (result) {
-                    //fprintf(stderr, "%08I64X %08X\n", fat_offset, next_cluster);
-                    const uint64_t next_offset = this->cluster_to_offset(next_cluster);
-                    size = remains < this->cluster_size ? remains : this->cluster_size;
-                    result = this->file.copy_to(out, next_offset, size);
-                    remains -= size;
-                    cluster = next_cluster;
-                }
-            }
+        exFatFile in(*this);
+        if (in.open(se) == false) {
+            return false;
         }
-        return result;
+        return in.copy_to(out, se.data_length);
     }
-    void enum_files_worker(const wchar_t* parent, const wchar_t* outdir, const wchar_t* pattern, uint64_t offset, unsigned int level) const
+    void enum_files_worker(exFatFile& dir, const wchar_t* parent, const wchar_t* outdir, const wchar_t* pattern, unsigned int level) const
     {
         file_entry fe;
-        for (; this->file.read_at(offset, &fe, sizeof(entry)) && fe.type != 0; ) {
-            const uint64_t offent = offset;
-            offset += sizeof(entry);
+        for (; dir.read(&fe, sizeof(fe)) == sizeof(fe) && fe.type != 0; ) {
+            //const uint64_t offent = 0;  // TODO: fill this variable
             if ((fe.type & 0x7f) != 0x05) {
                 continue;
             }
             if ((fe.type & 0x80) == 0 || fe.secondary_count < 2 || (fe.attributes & ~0xffff) != 0) {
-                offset += fe.secondary_count * sizeof(entry);
+                if (dir.read(NULL, fe.secondary_count * sizeof(entry)) == false) {
+                    break;
+                }
                 continue;
             }
             const size_t cchMax = 256;
@@ -594,10 +719,9 @@ private:
             stream_extension se = {};
             for (uint_t i = 0; i < fe.secondary_count; i++) {
                 entry e;
-                if (this->file.read_at(offset, &e, sizeof(entry)) == false) {
+                if (dir.read(&e, sizeof(e)) == false) {
                     return;
                 }
-                offset += sizeof(entry);
                 if (e.type == 0xC0) {
                     if (se.type == 0) {
                         se = reinterpret_cast<const stream_extension&>(e);
@@ -616,26 +740,36 @@ private:
 #define TIMEFMT     "%04u-%02u-%02u %02u:%02u:%02u"
 #define TIME(t)     (t).year, (t).month, (t).day, (t).hour, (t).minute, (t).second
 #define ATTR(b, t)  ((fe.attributes & (b)) ? (L ## t) : L'.')
-            printf(TIMEFMT " %c%c%c%c%c %16I64X %12I64u %12u %16I64X  ",
+            printf(TIMEFMT " %c%c%c%c%c %12I64u %12u %16I64X  ",
                 TIME(tm),
                 ATTR(0x10, 'D'),
                 ATTR(0x20, 'A'),
                 ATTR(4, 'S'),
                 ATTR(2, 'H'),
                 ATTR(1, 'R'),
-                offent, se.data_length, se.first_cluster, file_offset);
+                //offent,
+                se.data_length, se.first_cluster, file_offset);
 #undef ATTR
+#undef TIME
+#undef TIMEFMT
             ::wprintf(L"%s", path);
             ::putchar('\n');
             if ((fe.attributes & 0x10) == 0x10) {
-                this->enum_files_worker(path, outdir, pattern, file_offset, level + 1);
-                set_file_info(path, outdir, fe, true);
+                exFatFile child(*this);
+                if (child.open(se)) {
+                    this->enum_files_worker(child, path, outdir, pattern, level + 1);
+                } else {
+                    fwprintf(stderr, L"Failed to open: %s\n", path);
+                }
+                if (outdir != NULL) {
+                    set_file_info(path, outdir, fe, true);
+                }
             } else if (outdir != NULL && (pattern == NULL || wildmatch(path, pattern))) {
                 if (extract_file(path, outdir, se)) {
                     set_file_info(path, outdir, fe, false);
-                    fwprintf(stderr, L"Saved: %s\n", path);
+                    _cwprintf(L"Saved: %s\n", path);
                 } else {
-                    fwprintf(stderr, L"FAIL: %s\n", path);
+                    _cwprintf(L"FAIL: %s\n", path);
                 }
             }
         }
